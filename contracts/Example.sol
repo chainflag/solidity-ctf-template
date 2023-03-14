e20dc99bd66c4c1a746e6fe6d10a49a88cea5e56
iczc <iczcalan@gmail.com>
2023-03-14 22:55:36 +0800

    add example contract

---
+++ contracts/Example.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.9;
+
+contract Greeter {
+    string greeting;
+
+    constructor(string memory _greeting) public {
+        greeting = _greeting;
+    }
+
+    function greet() public view returns (string memory) {
+        return greeting;
+    }
+
+    function setGreeting(string memory _greeting) public {
+        greeting = _greeting;
+    }
+
+    function isSolved() public view returns (bool) {
+        string memory expected = "HelloChainFlag";
+        return keccak256(abi.encodePacked(expected)) == keccak256(abi.encodePacked(greeting));
+    }
+}
